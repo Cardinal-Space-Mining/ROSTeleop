6cce737ad9a70a2fd6f0dc06aff8d06dd016057a
Jacob M <willmoiser@gmail.com>
2024-07-18 22:44:06 -0500

    Wrote a preliminary robot side

--- pkgs/robot/src/main.cpp
+++ pkgs/robot/src/main.cpp
@@ -1,13 +1,24 @@
+
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <span>
+#include <string>
+#include <thread>
+#include <utility>
+#include <vector>
+
+#include <unistd.h>
+
+#include <rclcpp/rclcpp.hpp>
+
 #define Phoenix_No_WPI // remove WPI dependencies
 #include "ctre/Phoenix.h"
 #include "ctre/phoenix/cci/Unmanaged_CCI.h"
 #include "ctre/phoenix/platform/Platform.h"
 #include "ctre/phoenix/unmanaged/Unmanaged.h"
-#include <chrono>
-#include <iostream>
-#include <string>
-#include <thread>
-#include <unistd.h>
+
+#include "custom_types/msg/talon_ctrl.hpp"
 
 using namespace ctre::phoenix;
 using namespace ctre::phoenix::platform;
@@ -16,19 +27,39 @@ using namespace ctre::phoenix::motorcontrol::can;
 
 using namespace std::chrono_literals;
 
-/* make some talons for drive train */
-TalonSRX talRght(0);
-
-void initDrive()
+class Robot : public rclcpp::Node
 {
-    /* both talons should blink green when driving forward */
-    talRght.SetInverted(true);
-}
+public:
+    template <size_t V>
+    Robot(std::span<const std::pair<const char *, int>, V> motors)
+    : rclcpp::Node("robot")
+    {
+        for(const auto & motor : motors)
+        {
+            auto & motor_ref =
+                m_motors.emplace_back(std::make_unique<TalonSRX>(motor.second));
+            m_motor_subs.emplace_back(this->create_subscription<
+                                      custom_types::msg::TalonCtrl>(
+                motor.first, 10,
+                [&motor_ref](const custom_types::msg::TalonCtrl & msg)
+                {
+                    motor_ref->Set(
+                        static_cast<ctre::phoenix::motorcontrol::ControlMode>(
+                            msg.mode),
+                        msg.value);
+                }));
+        }
+    }
 
-int main()
+private:
+    std::vector<std::unique_ptr<TalonSRX>> m_motors;
+    std::vector<
+        std::shared_ptr<rclcpp::Subscription<custom_types::msg::TalonCtrl>>>
+        m_motor_subs;
+};
+
+int main(int argc, char ** argv)
 {
-    /* don't bother prompting, just use can0 */
-    // std::cout << "Please input the name of your can interface: ";
     auto interface = "can0";
     if(ctre::phoenix::platform::can::SetCANInterface(interface) != 0)
     {
@@ -36,29 +67,17 @@ int main()
         std::exit(-1);
     }
 
-    // Comment out the call if you would rather use the automatically running
-    // diag-server, note this requires uninstalling diagnostics from Tuner.
-    // c_SetPhoenixDiagnosticsStartTime(-1); // disable diag server, instead we will use the diag server stand alone application that Tuner installs
+    static constexpr std::pair<const char *, int> motors[] = {
+        {"track_right", 0},
+        {"track_left", 1},
+        {"trencher", 2},
+        {"hopper_belt", 3},
+        {"hopper_actuator", 4}};
 
-    while(true)
-    {
-        double percent = 0;
-        for(size_t i = 0; i < 100; i++)
-        {
-            /* code */
-            talRght.Set(ControlMode::PercentOutput, percent);
-            percent += 1;
-            std::this_thread::sleep_for(1ms);
-        }
-
-        for(size_t i = 0; i < 100; i++)
-        {
-            /* code */
-            talRght.Set(ControlMode::PercentOutput, percent);
-            percent -= 1;
-            std::this_thread::sleep_for(1ms);
-        }
-    }
-
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<Robot>(
+        std::span{std::begin(motors), std::end(motors)});
+    rclcpp::spin(node);
+    rclcpp::shutdown();
     return 0;
 }
